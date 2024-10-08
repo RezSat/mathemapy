b955eed6efc06ea0ab9c67e701300dc7848de08b
Yehan Wasura <yehantest@gmail.com>
2024-10-08 13:05:01 +0530

    Update subtraction.py, untested.

--- mathemapy/core/subtraction.py
+++ mathemapy/core/subtraction.py
@@ -1,90 +1,116 @@
 from .operators import BinaryOperator
 from .numbers import Number
 from .symbol import Symbol
-from .multiplication import Multiplication
 from .node import Node
+from .multiplication import Multiplication
 
 class Subtraction(BinaryOperator):
     symbol = '-'
-    
+
     def __init__(self, left, right):
         super().__init__(left, right)
-        flat_terms = self._flattern(left, self._negate(right))
-        self.terms = self._collect_like_terms(flat_terms)
-
-    def _compare_same_type(self, other):
-        return isinstance(other, Subtraction) and self.terms == other.terms
+        self.left = left
+        self.right = right
 
     def evaluate(self):
-        evaluated_terms = [ term.evaluate() if isinstance(term, Node) else term for term in self.terms ]
-        numeric_diff = Number(sum([term for term in evaluated_terms if isinstance(term, (int, float))]))
+        # Evaluate left and right
+        evaluated_left = self.left.evaluate() if isinstance(self.left, Node) else self.left
+        evaluated_right = self.right.evaluate() if isinstance(self.right, Node) else self.right
 
-        remaining_terms = [ term for term in evaluated_terms if not isinstance(term, (int, float)) ]
-        if numeric_diff != 0:
-            remaining_terms.insert(0, numeric_diff)
-        if len(remaining_terms) == 1:
-            return remaining_terms[0]  # If only one term, return it
-        return self._group_as_binary_subtraction(remaining_terms)
+        # If both sides are numeric, return the numeric result
+        if isinstance(evaluated_left, Number) and isinstance(evaluated_right, Number):
+            return Number(evaluated_left.value - evaluated_right.value)
 
-    def _group_as_binary_subtraction(self, terms):
+        # If only the right is numeric and equals zero, return the left side
+        if isinstance(evaluated_right, Number) and evaluated_right.value == 0:
+            return evaluated_left
+
+        # Return a simplified Subtraction if no further simplification is possible
+        return Subtraction(evaluated_left, evaluated_right)
+
+    def _compare_same_type(self, other):
         """
-        Recursively groups terms into nested Subtraction nodes
-        Takes a list of terms and returns a Subtraction object that respects binary structure
+        Compare two Subtraction objects by checking if their left and right sides are equal.
         """
-        if len(terms) == 2:
-            return Subtraction(terms[0], terms[1])
-        else:
-            # Recursively group terms: Subtraction(left, Subtraction(remain terms))
-            return Subtraction(terms[0], self._group_as_binary_subtraction(terms[1:]))
+        if isinstance(other, Subtraction):
+            return (self.left == other.left) and (self.right == other.right)
+        return False
 
-    def _flattern(self, *terms):
-        operands = []
-        for term in terms:
-            if isinstance(term, Subtraction):
-                operands.extend(term._flattern(term.left, self._negate(term.right)))
-            else:
-                operands.append(term)
-        return operands
-
-    def _collect_like_terms(self, terms):
-        collected = {}
-        for term in terms:
-            if isinstance(term, Number):
-                collected['number'] = collected.get('number', 0) + term.value
-            elif isinstance(term, Symbol):
-                if term.name in collected:
-                    collected[term.name] += 1  # Increment coefficient
-                else:
-                    collected[term.name] = 1
-            elif isinstance(term, Multiplication):
-                base, coeff = term.left, term.right
-                if isinstance(base, Symbol) and isinstance(coeff, Number):
-                    if base.name in collected:
-                        collected[base.name] += coeff.value
-                    else:
-                        collected[base.name] = coeff.value
-            else:
-                collected[term] = collected.get(term, 0) + 1
-
-        # Rebuild terms based on collected results
-        result = []
-        if 'number' in collected and collected['number'] != 0:
-            result.append(Number(collected['number']))
-        for term, coeff in collected.items():
-            if term != 'number' and coeff != 0:
-                if coeff == 1:
-                    result.append(term)
-                else:
-                    result.append(Multiplication(Number(coeff), Symbol(term)))
-        return result
+    def __repr__(self):
+        return f"({repr(self.left)} - {repr(self.right)})"
 
     def _negate(self, term):
-        """Negates a term (used for subtraction)."""
+        """
+        Negate the term for handling subtraction, returns a -term expression.
+        """
         if isinstance(term, Number):
             return Number(-term.value)
         elif isinstance(term, Multiplication) and isinstance(term.left, Number):
+            # If we have something like -1 * x, just negate the number
             return Multiplication(Number(-term.left.value), term.right)
-        return Multiplication(Number(-1), term)
+        else:
+            # For all other terms, return -1 * term
+            return Multiplication(Number(-1), term)
 
-    def __repr__(self):
-        return " - ".join([repr(term) for term in self.terms])
+    def flatten(self):
+        """
+        Flattens nested subtractions: (a - (b - c)) -> (a - b + c).
+        This returns a list of terms.
+        """
+        terms = [self.left]
+
+        if isinstance(self.right, Subtraction):
+            terms.append(self.right.left)
+            negated_term = self._negate(self.right.right)
+            terms.append(negated_term)
+        else:
+            negated_right = self._negate(self.right)
+            terms.append(negated_right)
+
+        return terms
+
+    def collect_like_terms(self):
+        """
+        Collect like terms: a - a = 0, combine numeric terms, etc.
+        """
+        terms = self.flatten()
+
+        numeric_sum = 0
+        symbol_terms = {}
+
+        for term in terms:
+            if isinstance(term, Number):
+                numeric_sum += term.value
+            elif isinstance(term, Symbol):
+                if term.name in symbol_terms:
+                    symbol_terms[term.name] += 1
+                else:
+                    symbol_terms[term.name] = 1
+            else:
+                # Handle other cases like Multiplications
+                if term in symbol_terms:
+                    symbol_terms[term] += 1
+                else:
+                    symbol_terms[term] = 1
+
+        # Build the final result
+        result_terms = []
+        if numeric_sum != 0:
+            result_terms.append(Number(numeric_sum))
+
+        for symbol, coeff in symbol_terms.items():
+            if coeff == 1:
+                result_terms.append(symbol)
+            elif coeff > 1:
+                result_terms.append(Multiplication(Number(coeff), symbol))
+
+        # If there's only one term left, return it
+        if len(result_terms) == 1:
+            return result_terms[0]
+        
+        # Otherwise, return a nested Subtraction expression
+        result = result_terms[0]
+        for term in result_terms[1:]:
+            result = Subtraction(result, term)
+
+        return result
