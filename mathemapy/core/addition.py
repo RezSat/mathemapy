fea5ffb83a2fb5acac6b34968db21e2dc9d7c94b
Yehan Wasura <yehantest@gmail.com>
2024-10-20 19:20:21 +0530

    added addition and subtration classes

---
+++ mathemapy/core/addition.py
@@ -0,0 +1,8 @@
+class Addition:
+    def __init__(self, *args):
+        self.operands = args
+
+    def evaluate(self):
+        from 
+        # considering all are just integers and flaots
+        return sum(self.operands)
