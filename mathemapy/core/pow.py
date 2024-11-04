a603a6ec10b375313974c6cfd8dd1b945aafe573
Yehan Wasura <yehantest@gmail.com>
2024-11-04 19:02:41 +0530

    Update pow.py

--- mathemapy/core/pow.py
+++ mathemapy/core/pow.py
@@ -10,13 +10,38 @@ class Pow(Expression):
         eval_left = self.left.evaluate()
         eval_right = self.right.evaluate()
 
-        if isinstance(self.left, Number) and isinstance(self.right, Number):
+        if isinstance(eval_left, (int, float)) and isinstance(eval_right, (int, float)):
             return Number(eval_left ** eval_right)
-        else:
-            return Add(self.left, self.right)
 
+        return self.simplify()
+    
     def simplify(self):
-        pass # do something later
+        left_simple = self.left.simplify()
+        right_simple = self.right.simplify()
+        
+        # Handle numeric powers
+        if isinstance(left_simple, Number) and isinstance(right_simple, Number):
+            return Number(left_simple.value ** right_simple.value)
+        
+        # x^0 = 1
+        if isinstance(right_simple, Number) and right_simple.value == 0:
+            return Number(1)
+        
+        # x^1 = x
+        if isinstance(right_simple, Number) and right_simple.value == 1:
+            return left_simple
+        
+        # Handle nested powers: (x^a)^b = x^(a*b)
+        if isinstance(left_simple, Pow):
+            new_exp = Mul(left_simple.right, right_simple).simplify()
+            return Pow(left_simple.left, new_exp).simplify()
+        
+        # 1^x = 1
+        if isinstance(left_simple, Number) and left_simple.value == 1:
+            return Number(1)
+        
+        return Pow(left_simple, right_simple)
 
     def __str__(self):
-        return f'({self.left} ^ {self.right})'
+        return f"({self.left} ^ {self.right})"
+        
